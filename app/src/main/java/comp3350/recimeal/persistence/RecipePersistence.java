ea7f048e63584980bc1201da432d2f2dd59ff491
Haozhi Wang <wangh17@myumanitoba.ca>
2023-02-04 22:12:30 -0600

    Add interface methods for RecipePersistence

--- app/src/main/java/comp3350/recimeal/persistence/RecipePersistence.java
+++ app/src/main/java/comp3350/recimeal/persistence/RecipePersistence.java
@@ -1,2 +1,15 @@
-package comp3350.recimeal.persistence;public interface RecipePersistence {
+package comp3350.recimeal.persistence;
+
+import comp3350.recimeal.objects.Recipe;
+import java.util.List;
+
+public interface RecipePersistence {
+
+    List<Recipe> getRecipeSequential();
+
+    Recipe insertRecipe(Recipe newRecipe);
+
+    Recipe updateRecipe(Recipe newRecipe);
+
+    void deleteRecipe(Recipe discardRecipe);
 }
