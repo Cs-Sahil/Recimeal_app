3c2918569720e8ab23724b2c52687b167449f051
Sahil Sharma <sharm56@myumanitoba.ca>
2023-02-08 21:36:31 +0000

    Update app/src/main/java/comp3350/recimeal/persistence/stubs/RecipePersistenceStub.java

--- app/src/main/java/comp3350/recimeal/persistence/stubs/RecipePersistenceStub.java
+++ app/src/main/java/comp3350/recimeal/persistence/stubs/RecipePersistenceStub.java
@@ -8,9 +8,20 @@ import java.util.ArrayList;
 
 public class RecipePersistenceStub implements RecipePersistence{
     private List<Recipe> recipes;
+ 
+    private String friedRiceInstructions = "Add the cooked rice to the pan and stir until well combined with the vegetables      and eggs.\n" + "Cook for an additional 2-3 minutes, or until the rice is heated through.\n";
+   
+   private String omeleteIntructions = "Whisk 2 eggs, add fillings, cook both sides in pan. Serve hot.";
+   private String BrownieInstructions = "Mix melted chocolate and butter. Add sugar, eggs, and flour. Bake and serve.";
+    
 
     public RecipePersistenceStub() {
         this.recipes = new ArrayList<>();
+        
+        recipes.add(new Recipe("Fried Rice",friedRiceInstructions))
+        recipes.add(new Recipe("omelette",omeleteIntructions ))
+        recipes.add(new Recipe("Brownies", BrownieInstructions))
+        
     }
 
     @Override
@@ -31,7 +42,7 @@ public class RecipePersistenceStub implements RecipePersistence{
     public Recipe updateRecipe(Recipe currRecipe) {
         for (int i = 0; i < recipes.size(); i++) {
             Recipe recipe = recipes.get(i);
-            if (recipe.getId().equals(currRecipe.getId())) {
+                 if (recipe.equals(currRecipe)) {
                 recipes.set(i, currRecipe);
                 return currRecipe;
             }
@@ -44,7 +55,7 @@ public class RecipePersistenceStub implements RecipePersistence{
     public void deleteRecipe(Recipe discardRecipe) {
         for (int i = 0; i < recipes.size(); i++) {
             Recipe recipe = recipes.get(i);
-            if (recipe.getId().equals(discardRecipe.getId())) {
+            if (recipe.equals(discardRecipe)) {
                 recipes.remove(i);
                 break;
             }
