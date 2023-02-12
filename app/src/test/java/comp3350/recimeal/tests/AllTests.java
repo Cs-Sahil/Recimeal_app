71b4cbe2ed729ee419637b4a5ffc8676ed2f4b2c
Haozhi Wang <wangh17@myumanitoba.ca>
2023-02-11 23:33:11 -0600

    Add AllTests class

--- app/src/test/java/comp3350/recimeal/tests/AllTests.java
+++ app/src/test/java/comp3350/recimeal/tests/AllTests.java
@@ -3,11 +3,23 @@ package comp3350.recimeal.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+import comp3350.recimeal.business.AccessRecipes;
+import comp3350.recimeal.tests.business.AccessRecipeTest;
+import comp3350.recimeal.tests.objects.IngredientTest;
+import comp3350.recimeal.tests.objects.RecipeTest;
 
-})
-public class AllTests
-{
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                AccessRecipeTest.class,
+                IngredientTest.class,
+                RecipeTest.class
+        }
+)
+
+
+
+public class AllTests {
+
 
 }
