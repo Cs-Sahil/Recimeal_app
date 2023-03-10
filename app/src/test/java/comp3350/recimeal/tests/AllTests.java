75f501437e6ea2effd9aa23ec68d56e6dc779be3
Haozhi Wang <wangh17@myumanitoba.ca>
2023-03-10 15:30:38 -0600

    Last minute fixup

--- app/src/test/java/comp3350/recimeal/tests/AllTests.java
+++ app/src/test/java/comp3350/recimeal/tests/AllTests.java
@@ -3,7 +3,6 @@ package comp3350.recimeal.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import comp3350.recimeal.tests.business.AccessRecipeTest;
 import comp3350.recimeal.tests.business.VerifyRecipeTest;
 import comp3350.recimeal.tests.objects.IngredientTest;
 import comp3350.recimeal.tests.objects.RecipeTest;
@@ -11,7 +10,6 @@ import comp3350.recimeal.tests.objects.RecipeTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
         {
-                AccessRecipeTest.class,
                 IngredientTest.class,
                 RecipeTest.class,
                 VerifyRecipeTest.class
