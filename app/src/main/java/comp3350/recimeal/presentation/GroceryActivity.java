35893973fc61b1636b9eb8a315ab0dc73e64aa7a
Manjyot Kalkat <57817155+ManjyotK@users.noreply.github.com>
2023-04-03 13:54:01 -0500

    Created new grocery list activity.

---
+++ app/src/main/java/comp3350/recimeal/presentation/GroceryActivity.java
@@ -0,0 +1,17 @@
+package comp3350.recimeal.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import comp3350.recimeal.R;
+
+public class GroceryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grocery);
+    }
+}
