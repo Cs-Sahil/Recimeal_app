b83e09c6603075744b246d0a701ff4c821377256
Kieran Ketelsen <123786317+Ketalcorn@users.noreply.github.com>
2023-04-09 12:55:24 -0500

    access Recipes failing some tests

--- app/src/test/java/comp3350/recimeal/tests/business/AccessRecipesIT.java
+++ app/src/test/java/comp3350/recimeal/tests/business/AccessRecipesIT.java
@@ -14,7 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.recimeal.business.AccessRecipes;
+import comp3350.recimeal.objects.PermanentRecipe;
 import comp3350.recimeal.objects.Recipe;
+import comp3350.recimeal.objects.TempRecipe;
 import comp3350.recimeal.persistence.RecipePersistence;
 import comp3350.recimeal.persistence.hsqldb.RecipeDBPersistence;
 import comp3350.recimeal.tests.utils.TestUtils;
@@ -44,6 +46,26 @@ public class AccessRecipesIT {
         assertTrue( " should be ", recipes.size() == 6 );
         System.out.println("Ending GetRecipes Testing ");
     }
+    @Test
+    public void testAddRecipe()
+    {
+        final Recipe r = new TempRecipe("test","yum", "make it","style","type", true, false,"");
+        accessRecipes.addRecipe(r);
+        assertEquals(7, accessRecipes.getRecipes().size());
+    }
+    @Test
+    public void testDeleteRecipe()
+    {
+        List<Recipe> recipes;
+
+        recipes = accessRecipes.getRecipes();
+        final Recipe r = recipes.get(1);
+        assertEquals(6, recipes.size());
+        accessRecipes.deleteRecipe(r);
+        recipes = accessRecipes.getRecipes();
+        assertEquals(5, recipes.size());
+    }
+
     @After
     public void tearDown() {
         // reset DB
