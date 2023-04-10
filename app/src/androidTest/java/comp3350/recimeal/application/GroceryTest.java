26732b43fa20a702dd5ec1522c829a9cf3c5ff9b
Kieran Ketelsen <123786317+Ketalcorn@users.noreply.github.com>
2023-04-09 20:46:38 -0500

    partly working

--- app/src/androidTest/java/comp3350/recimeal/application/GroceryTest.java
+++ app/src/androidTest/java/comp3350/recimeal/application/GroceryTest.java
@@ -1,5 +1,6 @@
 package comp3350.recimeal.application;
 
+import static android.os.SystemClock.sleep;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -7,6 +8,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
@@ -16,18 +18,21 @@ import org.junit.runner.RunWith;
 
 import comp3350.recimeal.R;
 import comp3350.recimeal.presentation.GroceryActivity;
+import comp3350.recimeal.presentation.MainActivity;
 
 
 /*
 This class runs an automated acceptance tests for the Grocery List feature
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class GroceryTest {
     @Rule
-    public ActivityTestRule<GroceryActivity> activityRule = new ActivityTestRule<>(GroceryActivity.class);
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void testTitle() {
+        sleep(4500);
         Espresso.onView(withId(R.id.textRecipeTitle)).check(matches(withText(containsString("Grocery List"))));
     }
 
