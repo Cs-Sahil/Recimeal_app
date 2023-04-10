5abce24830e7c6b04b8bcf39ae8d6ba5c223994d
Manjyot Kalkat <57817155+ManjyotK@users.noreply.github.com>
2023-04-09 22:15:08 -0500

    Fixed the filter tests

--- app/src/androidTest/java/comp3350/recimeal/FilterTest.java
+++ app/src/androidTest/java/comp3350/recimeal/FilterTest.java
@@ -11,12 +11,12 @@ import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.CoreMatchers.anything;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,21 +37,47 @@ public class FilterTest {
 
     @Test
     public void filterTest() {
+        //mark the first two recipe as favorite
+        onData(anything()).inAdapterView(withId(R.id.listRecipes)).atPosition(0).perform(click());
+        onView(withId(R.id.favoriteSwitch)).perform(click());
+        sleep(1500);
+        onView(withId(R.id.home)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listRecipes)).atPosition(1).perform(click());
+        onView(withId(R.id.favoriteSwitch)).perform(click());
+        sleep(1500);
+        onView(withId(R.id.home)).perform(click());
 
         //click the favorite button to turn on the filter
         onView(withId(R.id.switchFav)).perform(click());
         //input nothing
         onView(withId(R.id.searchRecipeText)).perform(typeText(""));
-        closeSoftKeyboard();
-       //click the search button to apply the filter with empty search field to search all favorite recipes
-       onView(withId(R.id.searchRecipeButton)).perform(click());
+        //click the search button to apply the filter with empty search field to search all favorite recipes
+        onView(withId(R.id.searchRecipeButton)).perform(click());
+        //verify that first recipe is shown
+        onData(anything()).inAdapterView(withId(R.id.listRecipes)).atPosition(0).perform(click());
+        onView(withId(R.id.textRecipeTitle)).check(matches(withText("Spanish Rice and Beans")));
+        onView(withId(R.id.home)).perform(click());
 
-       //verify that first two recipes are shown
-       // onData(anything()).inAdapterView(withId(R.id.listRecipes)).atPosition(0).perform(click());
-       // onView(withId(R.id.textRecipeTitle)).check(matches(withText("Spanish Rice and Beans")));
-       // pressBack();
+        //click the favorite button to turn on the filter
+        onView(withId(R.id.switchFav)).perform(click());
+        //input nothing
+        onView(withId(R.id.searchRecipeText)).perform(typeText(""));
+        //click the search button to apply the filter with empty search field to search all favorite recipes
+        onView(withId(R.id.searchRecipeButton)).perform(click());
+        //verify that second recipes is shown
         onData(anything()).inAdapterView(withId(R.id.listRecipes)).atPosition(1).perform(click());
         onView(withId(R.id.textRecipeTitle)).check(matches(withText("Chicken Enchiladas")));
+        onView(withId(R.id.home)).perform(click());
+
+        // Unfavourite the recipes
+        onData(anything()).inAdapterView(withId(R.id.listRecipes)).atPosition(0).perform(click());
+        onView(withId(R.id.favoriteSwitch)).perform(click());
+        sleep(1500);
+        onView(withId(R.id.home)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.listRecipes)).atPosition(1).perform(click());
+        onView(withId(R.id.favoriteSwitch)).perform(click());
+        sleep(1500);
+        onView(withId(R.id.home)).perform(click());
 
     }
 
